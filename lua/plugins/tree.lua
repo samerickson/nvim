eb5290206dbcddc90475de7990ce395ae4bdcb9f
Sam Erickson <samericksn@gmail.com>
2023-07-30 09:55:10 -0700

    feat: add show help mapping to neotree
    This mapping matches that of telescope so there is one less keybinding
    for me to remember.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -6,6 +6,13 @@ return {
     { "nvim-lua/plenary.nvim" },
     { "nvim-tree/nvim-web-devicons" }, -- not strictly required, but recommended
     { "MunifTanjim/nui.nvim" }
+  },
+  opts = {
+    window = {
+      mappings = {
+        ["<C-_>"] = "show_help",
+      }
+    }
   }
 }
 
