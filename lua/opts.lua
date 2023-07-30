bce822f8a51fcc112005da4f0ae1d12ac09148d7
Sam Erickson <samericksn@gmail.com>
2023-07-30 14:12:40 -0700

    feat: add autocmd to highlight yank selection

--- lua/opts.lua
+++ lua/opts.lua
@@ -40,6 +40,18 @@ set.colorcolumn = "120"
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
+-- Highlight yank selection
+autocmd('TextYankPost', {
+    group = augroup('HighlightYank', {}),
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 120,
+        })
+    end,
+})
+
 -- Ensure help files always open as a vertical split
 autocmd("FileType", {
   pattern = "help",
