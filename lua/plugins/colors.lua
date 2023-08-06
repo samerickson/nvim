b1dd9643f0085103e2eac3ccb22f314a11843fc6
Sam Erickson <samericksn@gmail.com>
2023-08-05 17:38:51 -0700

    bug: fix telescope border backgrounds
    Backgrounds were not transparent, now they are.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -3,21 +3,23 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 900,
-    init = function ()
+    init = function()
       vim.cmd("colorscheme kanagawa")
+      vim.cmd("highlight TelescopeBorder guibg=none")
+      vim.cmd("highlight TelescopeTitle guibg=none")
     end,
     opts = {
-      compile = true,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
+      compile = true,   -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
       commentStyle = { italic = true },
       functionStyle = {},
-      keywordStyle = { italic = true},
+      keywordStyle = { italic = true },
       statementStyle = { bold = true },
       typeStyle = {},
-      transparent = true,         -- do not set background color
-      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
+      transparent = true,    -- do not set background color
+      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = {             -- add/modify theme and palette colors
         palette = {},
         theme = {
           wave = {},
@@ -30,15 +32,11 @@ return {
           }
         },
       },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = "wave",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-        dark = "wave",           -- try "dragon" !
+      theme = "wave",  -- Load "wave" theme when 'background' option is not set
+      background = {   -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
         light = "lotus"
       },
     }
   }
 }
-
