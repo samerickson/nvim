846ce42e41ea17ca1924a4db59e225643e1c0d86
Sam Erickson <samericksn@gmail.com>
2023-10-01 17:07:29 -0700

    feat: show popup for lsp suggestions/errors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -98,6 +98,9 @@ return {
       vim.diagnostic.config({
         virtual_text = true
       })
+
+      vim.o.updatetime = 250
+      vim.cmd [[ autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false}) ]]
     end
   }
 }
