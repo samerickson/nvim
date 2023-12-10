ff25ce3aa9594b36f4b3bc2fd72ba26c02d96c0b
Sam Erickson <samericksn@gmail.com>
2023-12-09 21:44:19 -0800

    feat: add explore alias to dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -13,14 +13,15 @@ return {
       config = {
         -- stylua: ignore
         center = {
-          { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua require("util").telescope()]], desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          { action = "Telescope find_files",              desc = " Find file",        icon = " ", key = "f" },
+          { action = "Explore",                           desc = " Explore",          icon = " ", key = "e" },
+          { action = "ene | startinsert",                 desc = " New file",         icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                desc = " Recent files",     icon = " ", key = "r" },
+          { action = "Telescope live_grep",               desc = " Find text",        icon = " ", key = "g" },
+          { action = [[lua require("util").telescope()]], desc = " Config",           icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()', desc = " Restore Session",  icon = " ", key = "s" },
+          { action = "Lazy",                              desc = " Lazy",             icon = "󰒲 ", key = "l" },
+          { action = "qa",                                desc = " Quit",             icon = " ", key = "q" },
         },
         footer = function()
           local stats = require("lazy").stats()
