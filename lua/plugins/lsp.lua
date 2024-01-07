85d65980c627efd6b9b96e1eaf1fa33719078a6c
Sam Erickson <samericksn@gmail.com>
2024-01-07 15:53:35 -0800

    fix: gopls not starting
    The issue was due to GOMODCACHE and is present in the gopls lsp:
        https://github.com/neovim/nvim-lspconfig/issues/2733

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,15 +24,24 @@ return {
     config = function()
       local lsp_zero = require('lsp-zero')
 
+      local util = require'lspconfig.util'
+
       require('mason').setup({})
       require('mason-lspconfig').setup({
-        ensure_installed = {'tsserver', 'rust_analyzer'},
+        ensure_installed = { 'tsserver', 'rust_analyzer' },
         handlers = {
           lsp_zero.default_setup,
           lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
           end,
+          gopls = function()
+            require("lspconfig").gopls.setup {
+              root_dir = function(fname)
+                return util.root_pattern 'go.work' (fname) or util.root_pattern('go.mod', '.git')(fname)
+              end,
+            }
+          end
         }
       })
 
