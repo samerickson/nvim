4c229494994d8e19c42c648a0c04bd16dbd8208d
Sam Erickson <erickson.samuel@outlook.com>
2024-04-07 09:20:27 -0700

    steal folke init function

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.env.VSCODE then
+  vim.g.vscode = true
+end
+
+require("config.lazy")({
+  debug = false,
+  profiling = {
+    loader = false,
+    require = false,
+  },
+})
