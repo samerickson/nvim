5100fc5d42732fc958aab363c219c8c98940d3cf
Sam Erickson <erickson.samuel@outlook.com>
2024-04-07 15:19:04 -0700

    add neogen for generating comment blocks

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ if vim.env.VSCODE then
   vim.g.vscode = true
 end
 
+
 require("config.lazy")({
   debug = false,
   profiling = {
---
+++ lua/plugins/coding.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "danymat/neogen",
+    event = "VeryLazy",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+  },
+}
