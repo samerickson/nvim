aaff7adb7d733e7ff23641dffcd2d1f76393eae9
Sam Erickson <erickson.samuel@outlook.com>
2024-04-12 20:40:35 -0700

    Correct neovim-node-host program name on windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,14 +6,17 @@
 local osname = vim.loop.os_uname().sysname
 local node_bin
 local home_dir
+local neovim_node_prog
 
 if osname == "Linux" then
   node_bin = "/.local/share/fnm/node-versions/v21.6.1/installation/bin"
   home_dir = os.getenv("HOME")
+  neovim_node_prog = "neovim-node-host"
   -- vim.opt.shell = "bash"
 elseif osname == "Windows_NT" then
   node_bin = "\\AppData\\Roaming\\fnm\\node-versions\\v21.6.1\\installation"
   home_dir = "C:" .. os.getenv("HOMEPATH")
+  neovim_node_prog = "neovim-node-host.cmd"
   vim.opt.shell = "pwsh"
   vim.opt.shellcmdflag = "-c"
   vim.opt.shellquote = ""
@@ -24,8 +27,8 @@ local node_path = home_dir .. node_bin
 
 if require("samerickson.util").os.exists(node_path) then
   -- https://jaketrent.com/post/set-node-version-nvim/
-  vim.g.node_host_prog = node_path .. "/neovim-node-host"
-  vim.cmd("let $PATH = '" .. node_path .. ";' . $PATH")
+  vim.g.node_host_prog = node_path .. "/" .. neovim_node_prog
+  -- vim.cmd("let $PATH = '" .. node_path .. ";' . $PATH")
 else
   error("Node version v21.6.1 is not installed.")
 end
