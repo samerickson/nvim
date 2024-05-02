7e53fe4d7d2791e68b6b115f86f9cf712f1ad91f
Sam Erickson <erickson.samuel@outlook.com>
2024-05-01 20:35:24 -0700

    feat: improve markdown support

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
+}
