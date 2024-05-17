c18e7e012b380d7a1025d6e5a459ecf250320e06
Sam Erickson <samericksn@gmail.com>
2024-05-16 18:40:40 -0700

    feat: remove neovide configuration

--- init.lua
+++ init.lua
@@ -2,13 +2,6 @@ if vim.env.VSCODE then
   vim.g.vscode = true
 end
 
-if vim.g.neovide then
-  -- Put anything you want to happen only in Neovide here
-  -- vim.g.neovide_fullscreen = true
-  vim.g.neovide_cursor_animation_length = 0.03
-  vim.o.guifont = "CaskaydiaCove_NF:h13"
-end
-
 require("config.lazy")({
   debug = false,
   profiling = {
