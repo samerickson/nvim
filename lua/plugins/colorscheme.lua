025cc6551be760447b5bb04cc46042cff3e3faad
Sam Erickson <samericksn@gmail.com>
2024-06-01 08:02:35 -0700

    feat(colorscheme): Switch color scheme to ayu-mirage

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "Shatur/neovim-ayu" },
+  { "catppuccin/nvim" },
+  { "rebelot/kanagawa.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu-mirage",
+    },
+  },
+}
