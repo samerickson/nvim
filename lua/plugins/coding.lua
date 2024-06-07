b5d8b526de7112365eb417a50f8d32f88e1d2d38
Sam Erickson <samericksn@gmail.com>
2024-06-07 08:18:54 -0700

    feat(neogen): use upstream neogen

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "danymat/neogen",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-    keys = {
-      { "gc", "<cmd>Neogen<cr>", desc = "Generate documentation" },
-    },
-  },
-  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {},
