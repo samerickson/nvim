fb9aad487c24c823b9896508a44baafb729fefc9
Sam Erickson <samericksn@gmail.com>
2024-10-07 14:30:19 -0700

    feat: add keymaps for eol and bol in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,6 @@ map({ "i" }, "jk", "<Esc>", { silent = true })
 
 -- Make the windows clipboard manager work in neovim
 map({ "i" }, "<C-v>", "<C-r>*", { silent = true })
+
+map({ "i" }, "<C-e>", "<Esc>$a", { silent = true })
+map({ "i" }, "<C-b>", "<Esc>^i", { silent = true })
