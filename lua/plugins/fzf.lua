be170f3409eb1883e5fe1a02bd17ecb329120c54
Sam Erickson <samericksn@gmail.com>
2024-11-05 21:22:13 -0800

    feat: add keybindings for grep in fzf-lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -15,6 +15,8 @@ return {
         keys = {
             { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Find files' },
             { '<leader><space>', '<cmd>FzfLua files<cr>', desc = 'Find files' },
+            { '<leader>fg', '<cmd>FzfLua live_grep_glob<cr>', desc = 'Grep' },
+            { '<leader>/', '<cmd>FzfLua live_grep_glob<cr>', desc = 'Grep' },
         }
     }
 }
