b3a54b53ecbdacf8ed8532428de0e0b6626091f4
Sam Erickson <samericksn@gmail.com>
2024-11-18 19:04:49 -0800

    feat: add debug keybindings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -116,6 +116,16 @@ return {
     },
   },
   {
+    "mfussenegger/nvim-dap",
+    -- stylua: ignore
+    keys = {
+      { "<F9>", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<S-F9>", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<F5>", function() require("dap").continue() end, desc = "Run/Continue" }
+    },
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
