4276497a7a21442ffb093d02760252438b4a285f
Sam Erickson <samericksn@gmail.com>
2025-02-08 19:15:04 -0800

    chore(deps): remove unused

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 10000,
-    },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    branch = "main",
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
