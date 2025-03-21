38d74aea3c3d94bff65a919d6447aad833050186
Sam Erickson <erickson.samuel@outlook.com>
2025-03-20 20:18:21 -0700

    feat: support eslint flat config

--- lua/plugins/eslint.lua
+++ lua/plugins/eslint.lua
@@ -1,7 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = { eslint = {} },
+    servers = {
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = "auto" },
+          experimental = {
+            -- allows to use flat config format
+            useFlatConfig = true,
+          },
+        },
+      },
+    },
     setup = {
       eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
