57ef15c8f4b86621a1cb5136dd469757d1475640
Sam Erickson <erickson.samuel@outlook.com>
2025-07-26 20:01:57 -0700

    fix: lsp configuration not loading

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -212,84 +212,155 @@ return {
                 },
             }
 
-            -- LSP servers and clients are able to communicate to each other what features they support.
-            --  By default, Neovim doesn't support everything that is in the LSP specification.
-            --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-            --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-            -- Enable the following language servers
-            --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-            --
-            --  Add any additional override configuration in the following tables. Available keys are:
-            --  - cmd (table): Override the default command used to start the server
-            --  - filetypes (table): Override the default list of associated filetypes for the server
-            --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-            --  - settings (table): Override the default settings passed when initializing the server.
-            --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
-                -- gopls = {},
-                -- pyright = {},
-                -- rust_analyzer = {},
-                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                --
-                -- Some languages (like typescript) have entire language plugins that can be useful:
-                --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`ts_ls`) will work just fine
-                -- ts_ls = {},
-                --
-
-                lua_ls = {
-                    -- cmd = { ... },
-                    -- filetypes = { ... },
-                    -- capabilities = {},
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = 'Replace',
+                mason = {
+                    clangd = {},
+                    gopls = {},
+                    -- pyright = {},
+                    -- rust_analyzer = {},
+                    -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+                    --
+                    -- Some languages (like typescript) have entire language plugins that can be useful:
+                    --    https://github.com/pmizio/typescript-tools.nvim
+                    --
+                    -- But for many setups, the LSP (`ts_ls`) will work just fine
+                    -- ts_ls = {},
+                    --
+                    vtsls = {
+                        filetypes = {
+                            'javascript',
+                            'javascriptreact',
+                            'javascript.jsx',
+                            'typescript',
+                            'typescriptreact',
+                            'typescript.tsx',
+                            'vue',
+                        },
+                        settings = {
+                            complete_function_calls = true,
+                            vtsls = {
+                                enableMoveToFileCodeAction = true,
+                                autoUseWorkspaceTsdk = true,
+                                experimental = {
+                                    maxInlayHintLength = 30,
+                                    completion = {
+                                        enableServerSideFuzzyMatch = true,
+                                    },
+                                },
                             },
-                            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                            -- diagnostics = { disable = { 'missing-fields' } },
+                            typescript = {
+                                updateImportsOnFileMove = { enabled = 'always' },
+                                suggest = {
+                                    completeFunctionCalls = true,
+                                },
+                                inlayHints = {
+                                    enumMemberValues = { enabled = true },
+                                    functionLikeReturnTypes = { enabled = true },
+                                    parameterNames = { enabled = 'literals' },
+                                    parameterTypes = { enabled = true },
+                                    propertyDeclarationTypes = { enabled = true },
+                                    variableTypes = { enabled = false },
+                                },
+                            },
+                        },
+                        keys = {
+                            -- {
+                            --     'gD',
+                            --     function()
+                            --         local params = vim.lsp.util.make_position_params()
+                            --         LazyVim.lsp.execute {
+                            --             command = 'typescript.goToSourceDefinition',
+                            --             arguments = { params.textDocument.uri, params.position },
+                            --             open = true,
+                            --         }
+                            --     end,
+                            --     desc = 'Goto Source Definition',
+                            -- },
+                            -- {
+                            --     'gR',
+                            --     function()
+                            --         LazyVim.lsp.execute {
+                            --             command = 'typescript.findAllFileReferences',
+                            --             arguments = { vim.uri_from_bufnr(0) },
+                            --             open = true,
+                            --         }
+                            --     end,
+                            --     desc = 'File References',
+                            -- },
+                            -- {
+                            --     '<leader>co',
+                            --     LazyVim.lsp.action['source.organizeImports'],
+                            --     desc = 'Organize Imports',
+                            -- },
+                            -- {
+                            --     '<leader>cM',
+                            --     LazyVim.lsp.action['source.addMissingImports.ts'],
+                            --     desc = 'Add missing imports',
+                            -- },
+                            -- {
+                            --     '<leader>cu',
+                            --     LazyVim.lsp.action['source.removeUnused.ts'],
+                            --     desc = 'Remove unused imports',
+                            -- },
+                            -- {
+                            --     '<leader>cD',
+                            --     LazyVim.lsp.action['source.fixAll.ts'],
+                            --     desc = 'Fix all diagnostics',
+                            -- },
+                            -- {
+                            --     '<leader>cV',
+                            --     function()
+                            --         LazyVim.lsp.execute { command = 'typescript.selectTypeScriptVersion' }
+                            --     end,
+                            --     desc = 'Select TS workspace version',
+                            -- },
+                        },
+                    },
+
+                    lua_ls = {
+                        -- cmd = { ... },
+                        -- filetypes = { ... },
+                        -- capabilities = {},
+                        settings = {
+                            Lua = {
+                                completion = {
+                                    callSnippet = 'Replace',
+                                },
+                                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                                -- diagnostics = { disable = { 'missing-fields' } },
+                            },
                         },
                     },
                 },
+                others = {
+                    -- dartls = {},
+                },
             }
 
-            -- Ensure the servers and tools above are installed
-            --
-            -- To check the current status of installed tools and/or manually install
-            -- other tools, you can run
-            --    :Mason
-            --
-            -- You can press `g?` for help in this menu.
-            --
-            -- `mason` had to be setup earlier: to configure its options see the
-            -- `dependencies` table for `nvim-lspconfig` above.
-            --
-            -- You can add other tools here that you want Mason to install
-            -- for you, so that they are available from within Neovim.
-            local ensure_installed = vim.tbl_keys(servers or {})
+            local ensure_installed = vim.tbl_keys(servers.mason or {})
             vim.list_extend(ensure_installed, {
                 'stylua', -- Used to format Lua code
             })
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+            -- Either merge all additional server configs from the `servers.mason` and `servers.others` tables
+            -- to the default language server configs as provided by nvim-lspconfig or
+            -- define a custom server config that's unavailable on nvim-lspconfig.
+            for server, config in pairs(vim.tbl_extend('keep', servers.mason, servers.others)) do
+                if not vim.tbl_isempty(config) then
+                    vim.lsp.config(server, config)
+                end
+            end
+
             require('mason-lspconfig').setup {
                 ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-                automatic_installation = false,
-                handlers = {
-                    function(server_name)
-                        local server = servers[server_name] or {}
-                        -- This handles overriding only values explicitly passed
-                        -- by the server configuration above. Useful when disabling
-                        -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        require('lspconfig')[server_name].setup(server)
-                    end,
-                },
+                automatic_enable = true, -- automatically run vim.lsp.enable() for all servers that are installed via Mason
             }
+
+            -- Manually run vim.lsp.enable for all language servers that are *not* installed via Mason
+            if not vim.tbl_isempty(servers.others) then
+                vim.lsp.enable(vim.tbl_keys(servers.others))
+            end
         end,
     },
 }
