e99b1415e116b5033e02768a7b293ca9e5cc9e3e
Sam Erickson <erickson.samuel@outlook.com>
2025-07-26 20:06:59 -0700

    reafactor: clean up supports method calls
    client.supports_method is depricated, so falling back to it no longer
    makes sense.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,19 +101,6 @@ return {
                     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                     map('<leader>cr', vim.lsp.buf.rename, 'Rename')
 
-                    -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-                    ---@param client vim.lsp.Client
-                    ---@param method vim.lsp.protocol.Method
-                    ---@param bufnr? integer some lsp support methods only in specific files
-                    ---@return boolean
-                    local function client_supports_method(client, method, bufnr)
-                        if vim.fn.has 'nvim-0.11' == 1 then
-                            return client:supports_method(method, bufnr)
-                        else
-                            return client.supports_method(method, { bufnr = bufnr })
-                        end
-                    end
-
                     -- The following two autocommands are used to highlight references of the
                     -- word under your cursor when your cursor rests there for a little while.
                     --    See `:help CursorHold` for information about when this is executed
@@ -122,11 +109,7 @@ return {
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     if
                         client
-                        and client_supports_method(
-                            client,
-                            vim.lsp.protocol.Methods.textDocument_documentHighlight,
-                            event.buf
-                        )
+                        and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
                     then
                         local highlight_augroup =
                             vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
@@ -157,7 +140,7 @@ return {
                     -- This may be unwanted, since they displace some of your code
                     if
                         client
-                        and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+                        and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
                     then
                         map('<leader>th', function()
                             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
@@ -166,11 +149,7 @@ return {
 
                     if
                         client
-                        and client_supports_method(
-                            client,
-                            vim.lsp.protocol.Methods.textDocument_signatureHelp,
-                            event.buf
-                        )
+                        and client:supports_method(vim.lsp.protocol.Methods.textDocument_signatureHelp, event.buf)
                     then
                         map('<C-s>', function()
                             return vim.lsp.buf.signature_help()
