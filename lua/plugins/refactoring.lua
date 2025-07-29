df2b9e96743179b69175060bddac82fc5303bfa1
Sam Erickson <erickson.samuel@outlook.com>
2025-07-28 21:17:40 -0700

    fix: refactoring keybindings not working

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -6,84 +6,6 @@ return {
             'nvim-lua/plenary.nvim',
             'nvim-treesitter/nvim-treesitter',
         },
-        keys = {
-            { '<leader>r', '', desc = '+refactor', mode = { 'n', 'v' } },
-            {
-                '<leader>ri',
-                function()
-                    require('refactoring').refactor 'Inline Variable'
-                end,
-                mode = { 'n', 'v' },
-                desc = 'Inline Variable',
-            },
-            {
-                '<leader>rb',
-                function()
-                    require('refactoring').refactor 'Extract Block'
-                end,
-                desc = 'Extract Block',
-            },
-            {
-                '<leader>rf',
-                function()
-                    require('refactoring').refactor 'Extract Block To File'
-                end,
-                desc = 'Extract Block To File',
-            },
-            {
-                '<leader>rP',
-                function()
-                    require('refactoring').debug.printf { below = false }
-                end,
-                desc = 'Debug Print',
-            },
-            {
-                '<leader>rp',
-                function()
-                    require('refactoring').debug.print_var { normal = true }
-                end,
-                desc = 'Debug Print Variable',
-            },
-            {
-                '<leader>rc',
-                function()
-                    require('refactoring').debug.cleanup {}
-                end,
-                desc = 'Debug Cleanup',
-            },
-            {
-                '<leader>rf',
-                function()
-                    require('refactoring').refactor 'Extract Function'
-                end,
-                mode = 'v',
-                desc = 'Extract Function',
-            },
-            {
-                '<leader>rF',
-                function()
-                    require('refactoring').refactor 'Extract Function To File'
-                end,
-                mode = 'v',
-                desc = 'Extract Function To File',
-            },
-            {
-                '<leader>rx',
-                function()
-                    require('refactoring').refactor 'Extract Variable'
-                end,
-                mode = 'v',
-                desc = 'Extract Variable',
-            },
-            {
-                '<leader>rp',
-                function()
-                    require('refactoring').debug.print_var()
-                end,
-                mode = 'v',
-                desc = 'Debug Print Variable',
-            },
-        },
         opts = {
             prompt_func_return_type = {
                 go = false,
@@ -107,5 +29,29 @@ return {
             print_var_statements = {},
             show_success_message = true,
         },
+        config = function()
+            vim.keymap.set({ 'n', 'x' }, '<leader>re', function()
+                return require('refactoring').refactor 'Extract Function'
+            end, { expr = true })
+            vim.keymap.set({ 'n', 'x' }, '<leader>rf', function()
+                return require('refactoring').refactor 'Extract Function To File'
+            end, { expr = true })
+            vim.keymap.set({ 'n', 'x' }, '<leader>rv', function()
+                return require('refactoring').refactor 'Extract Variable'
+            end, { expr = true })
+            vim.keymap.set({ 'n', 'x' }, '<leader>rI', function()
+                return require('refactoring').refactor 'Inline Function'
+            end, { expr = true })
+            vim.keymap.set({ 'n', 'x' }, '<leader>ri', function()
+                return require('refactoring').refactor 'Inline Variable'
+            end, { expr = true })
+
+            vim.keymap.set({ 'n', 'x' }, '<leader>rbb', function()
+                return require('refactoring').refactor 'Extract Block'
+            end, { expr = true })
+            vim.keymap.set({ 'n', 'x' }, '<leader>rbf', function()
+                return require('refactoring').refactor 'Extract Block To File'
+            end, { expr = true })
+        end,
     },
 }
