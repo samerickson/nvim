480d10c5650e6e41b2e6a318e115523465f85366
Sam Erickson <samericksn@gmail.com>
2025-08-27 15:15:24 -0700

    feat(keymap): add keymaps for moving in insert mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,6 +9,11 @@ map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
 map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
 map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
 
+map('i', '<C-h>', '<Left>')
+map('i', '<C-j>', '<Up>')
+map('i', '<C-k>', '<Down>')
+map('i', '<C-l>', '<Right>')
+
 -- Resize window using <ctrl> arrow keys
 map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
 map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
