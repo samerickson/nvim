85ae279a01194edb51bfbf5f97eceb15bcbb55c9
Sam Erickson <erickson.samuel@outlook.com>
2025-09-25 08:54:59 -0700

    feat(yanky): increase history and highlight timer

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -1,8 +1,8 @@
 return {
     'gbprod/yanky.nvim',
     opts = {
-        ring = { history_length = 20 },
-        highlight = { timer = 150 },
+        ring = { history_length = 50 },
+        highlight = { timer = 250 },
     },
     keys = {
         {
