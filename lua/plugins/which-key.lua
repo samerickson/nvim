018dde059b4d44ed322c3d3d03ffdba97154e5bb
Sam Erickson <erickson.samuel@outlook.com>
2025-09-25 21:03:10 -0700

    feat(which-key): better keybinding groups
    Shamelessly stollen from
    https://github.com/LazyVim/LazyVim/blob/c83df9e68dd41f5a3f7df5a7048169ee286a7da8/lua/lazyvim/plugins/editor.lua#L59

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,5 +1,46 @@
 return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+        -- preset = 'helix',
+        spec = {
+            {
+                mode = { 'n', 'v' },
+                { '<leader><tab>', group = 'tabs' },
+                { '<leader>c', group = 'code' },
+                { '<leader>d', group = 'debug' },
+                { '<leader>dp', group = 'profiler' },
+                { '<leader>f', group = 'find' },
+                { '<leader>g', group = 'git' },
+                { '<leader>gh', group = 'hunks' },
+                { '<leader>q', group = 'quit' },
+                { '<leader>s', group = 'search' },
+                { '<leader>u', group = 'ui' },
+                { '<leader>x', group = 'quickfix' },
+                { '<leader>r', group = 'refactor' },
+                { '[', group = 'prev' },
+                { ']', group = 'next' },
+                { 'g', group = 'goto' },
+                { 'gs', group = 'surround' },
+                { 'z', group = 'fold' },
+                {
+                    '<leader>b',
+                    group = 'buffer',
+                    expand = function()
+                        return require('which-key.extras').expand.buf()
+                    end,
+                },
+                {
+                    '<leader>w',
+                    group = 'windows',
+                    proxy = '<c-w>',
+                    expand = function()
+                        return require('which-key.extras').expand.win()
+                    end,
+                },
+                -- better descriptions
+                { 'gx', desc = 'Open with system app' },
+            },
+        },
+    },
 }
