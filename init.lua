8bec3d21576ee9b6f93a647fa643eb9ef040299f
Sam Erickson <erickson.samuel@outlook.com>
2025-09-30 09:05:31 -0700

    feat: apply changes to init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+if not vim.uv or vim.loop.fs_stat(lazypath) then
+    -- stylua: ignore
+    vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require 'options'
@@ -14,16 +14,40 @@ require('lazy').setup {
     spec = {
         { import = 'plugins' },
     },
-    change_detection = { notify = false },
-    install = { colorscheme = { 'kanagawa' } },
     defaults = {
         lazy = true,
         version = false,
+    },
+    install = {
+        missing = true,
+        colorscheme = { 'kanagawa', 'habamax' },
+    },
+    checker = { enabled = false },
+    change_detection = {
+        notify = false,
     },
     dev = {
         path = '~/dev/nvim-plugins',
         patterns = { 'samerickson' },
         fallback = true,
     },
-    checker = { enabled = false },
+    performance = {
+        cache = { enabled = true },
+        reset_packpath = true,
+        rtp = {
+            disabled_plugins = {
+                '2html_plugin',
+                'gzip',
+                'matchit',
+                'matchparen',
+                'netrwPlugin',
+                'netrwSettings',
+                'netrwFileHandlers',
+                'tarPlugin',
+                'tohtml',
+                'tutor',
+                'zipPlugin',
+            },
+        },
+    },
 }
