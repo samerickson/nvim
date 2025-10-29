c1eb2ca138478c0e69e53044034d83b6c22cf0c2
Sam Erickson <erickson.samuel@outlook.com>
2025-10-28 20:34:26 -0700

    feat: clean up snack keybindings
    I want to use the default key bindings when possible

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -342,51 +342,51 @@ return {
                 desc = 'Colorschemes',
             },
             -- LSP
+            -- {
+            --     'gd',
+            --     function()
+            --         Snacks.picker.lsp_definitions()
+            --     end,
+            --     desc = 'Goto Definition',
+            -- },
+            -- {
+            --     'gD',
+            --     function()
+            --         Snacks.picker.lsp_declarations()
+            --     end,
+            --     desc = 'Goto Declaration',
+            -- },
+            -- {
+            --     'gr',
+            --     function()
+            --         Snacks.picker.lsp_references()
+            --     end,
+            --     nowait = true,
+            --     desc = 'References',
+            -- },
+            -- {
+            --     'gI',
+            --     function()
+            --         Snacks.picker.lsp_implementations()
+            --     end,
+            --     desc = 'Goto Implementation',
+            -- },
+            -- {
+            --     'gy',
+            --     function()
+            --         Snacks.picker.lsp_type_definitions()
+            --     end,
+            --     desc = 'Goto T[y]pe Definition',
+            -- },
             {
-                'gd',
-                function()
-                    Snacks.picker.lsp_definitions()
-                end,
-                desc = 'Goto Definition',
-            },
-            {
-                'gD',
-                function()
-                    Snacks.picker.lsp_declarations()
-                end,
-                desc = 'Goto Declaration',
-            },
-            {
-                'gr',
-                function()
-                    Snacks.picker.lsp_references()
-                end,
-                nowait = true,
-                desc = 'References',
-            },
-            {
-                'gI',
-                function()
-                    Snacks.picker.lsp_implementations()
-                end,
-                desc = 'Goto Implementation',
-            },
-            {
-                'gy',
-                function()
-                    Snacks.picker.lsp_type_definitions()
-                end,
-                desc = 'Goto T[y]pe Definition',
-            },
-            {
-                'gai',
+                'gri',
                 function()
                     Snacks.picker.lsp_incoming_calls()
                 end,
                 desc = 'C[a]lls Incoming',
             },
             {
-                'gao',
+                'gro',
                 function()
                     Snacks.picker.lsp_outgoing_calls()
                 end,
