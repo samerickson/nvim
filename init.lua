dd88432b1b4412769d7c5e5c05bbc5563e0dede6
Sam Erickson <erickson.samuel@outlook.com>
2025-12-03 19:34:50 -0800

    play with pack

--- init.lua
+++ init.lua
@@ -1,53 +1,7 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-
-if not vim.uv or vim.loop.fs_stat(lazypath) then
-    -- stylua: ignore
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
-end
-
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
 require 'options'
 require 'keymaps'
 
-require('lazy').setup {
-    spec = {
-        { import = 'plugins' },
-    },
-    defaults = {
-        lazy = true,
-        version = false,
-    },
-    install = {
-        missing = true,
-        colorscheme = { 'kanagawa', 'habamax' },
-    },
-    checker = { enabled = false },
-    change_detection = {
-        notify = false,
-    },
-    dev = {
-        path = '~/dev/nvim-plugins',
-        patterns = { 'samerickson' },
-        fallback = true,
-    },
-    performance = {
-        cache = { enabled = true },
-        reset_packpath = true,
-        rtp = {
-            disabled_plugins = {
-                '2html_plugin',
-                'gzip',
-                'matchit',
-                'matchparen',
-                'netrwPlugin',
-                'netrwSettings',
-                'netrwFileHandlers',
-                'tarPlugin',
-                'tohtml',
-                'tutor',
-                'zipPlugin',
-            },
-        },
-    },
-}
+require 'plugins.colorscheme'
+require 'plugins.snacks'
+
+vim.lsp.enable { 'lua_ls' }
---
+++ lua/plugin.back/colorscheme.lua
@@ -0,0 +1,108 @@
+return {
+    {
+        'rebelot/kanagawa.nvim',
+        lazy = false,
+        priority = 1000,
+        build = ':KanagawaCompile',
+        opts = {
+            compile = true, -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true },
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = false, -- do not set background color
+            dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true, -- define vim.g.terminal_color_{0,17}
+            colors = { -- add/modify theme and palette colors
+                palette = {},
+                theme = {
+                    wave = {},
+                    lotus = {},
+                    dragon = {},
+                    all = {
+                        ui = {
+                            bg_gutter = 'none',
+                        },
+                    },
+                },
+            },
+            overrides = function(colors) -- add/modify highlights
+                local theme = colors.theme
+                local highlights = {
+                    NormalFloat = { bg = 'none' },
+                    FloatBorder = { bg = 'none' },
+                    FloatTitle = { bg = 'none' },
+
+                    -- SnacksDashboard
+                    SnacksDashboardHeader = { fg = theme.vcs.removed },
+                    SnacksDashboardFooter = { fg = theme.syn.comment },
+                    SnacksDashboardDesc = { fg = theme.syn.identifier },
+                    SnacksDashboardIcon = { fg = theme.ui.special },
+                    SnacksDashboardKey = { fg = theme.syn.special1 },
+                    SnacksDashboardSpecial = { fg = theme.syn.comment },
+                    SnacksDashboardDir = { fg = theme.syn.identifier },
+                    -- SnacksProfiler
+                    SnacksProfilerIconInfo = { bg = theme.ui.bg_search, fg = theme.syn.fun },
+                    SnacksProfilerBadgeInfo = { bg = theme.ui.bg_visual, fg = theme.syn.fun },
+                    SnacksScratchKey = { link = 'SnacksProfilerIconInfo' },
+                    SnacksScratchDesc = { link = 'SnacksProfilerBadgeInfo' },
+                    SnacksProfilerIconTrace = { bg = theme.syn.fun, fg = theme.ui.float.fg_border },
+                    SnacksProfilerBadgeTrace = { bg = theme.syn.fun, fg = theme.ui.float.fg_border },
+                    SnacksIndent = { fg = theme.ui.bg_p2, nocombine = true },
+                    SnacksIndentScope = { fg = theme.ui.pmenu.bg, nocombine = true },
+                    SnacksZenIcon = { fg = theme.syn.statement },
+                    SnacksInputIcon = { fg = theme.ui.pmenu.bg },
+                    SnacksInputBorder = { fg = theme.syn.identifier },
+                    SnacksInputTitle = { fg = theme.syn.identifier },
+                    -- SnacksPicker
+                    SnacksPickerInputBorder = { fg = theme.syn.constant },
+                    SnacksPickerInputTitle = { fg = theme.syn.constant },
+                    SnacksPickerBoxTitle = { fg = theme.syn.constant },
+                    SnacksPickerSelected = { fg = theme.syn.number },
+                    SnacksPickerToggle = { link = 'SnacksProfilerBadgeInfo' },
+                    SnacksPickerPickWinCurrent = { fg = theme.ui.fg, bg = theme.syn.number, bold = true },
+                    SnacksPickerPickWin = { fg = theme.ui.fg, bg = theme.ui.bg_search, bold = true },
+                    SnacksPickerTree = { bg = 'none' },
+
+                    -- Save an hlgroup with dark background and dimmed foreground
+                    -- so that you can use it where your still want darker windows.
+                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                    -- Popular plugins that open floats will link to NormalFloat by default;
+                    -- set their background accordingly if you wish to keep them dark and borderless
+                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                }
+
+                -- SnacksNotifier
+                local notifier_links = {
+                    { suffix = 'Error', target = 'DiagnosticError' },
+                    { suffix = 'Warn', target = 'DiagnosticWarn' },
+                    { suffix = 'Info', target = 'DiagnosticInfo' },
+                    { suffix = 'Debug', target = 'Debug' },
+                    { suffix = 'Trace', target = 'Comment' },
+                }
+
+                for _, link in ipairs(notifier_links) do
+                    highlights['SnacksNotifierBorder' .. link.suffix] = { link = link.target }
+                    highlights['SnacksNotifierIcon' .. link.suffix] = { link = link.target }
+                    highlights['SnacksNotifierTitle' .. link.suffix] = { link = link.target }
+                    highlights['SnacksNotifier' .. link.suffix] = { link = link.target }
+                end
+
+                return highlights
+            end,
+            theme = 'wave',
+            background = {
+                dark = 'wave',
+                light = 'lotus',
+            },
+        },
+        init = function()
+            vim.cmd 'colorscheme kanagawa'
+        end,
+    },
+}
