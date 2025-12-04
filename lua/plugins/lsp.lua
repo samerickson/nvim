2800e734e62caac61101341546d79092bcd9ec1f
Sam Erickson <erickson.samuel@outlook.com>
2025-12-03 18:43:17 -0800

    feat: golsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -180,7 +180,39 @@ return {
         local servers = {
             mason = {
                 clangd = {},
-                gopls = {},
+                gopls = {
+                    gofumpt = true,
+                    codelenses = {
+                        gc_details = false,
+                        generate = true,
+                        regenerate_cgo = true,
+                        run_govulncheck = true,
+                        test = true,
+                        tidy = true,
+                        upgrade_dependency = true,
+                        vendor = true,
+                    },
+                    hints = {
+                        assignVariableTypes = true,
+                        compositeLiteralFields = true,
+                        compositeLiteralTypes = true,
+                        constantValues = true,
+                        functionTypeParameters = true,
+                        parameterNames = true,
+                        rangeVariableTypes = true,
+                    },
+                    analyses = {
+                        nilness = true,
+                        unusedparams = true,
+                        unusedwrite = true,
+                        useany = true,
+                    },
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    staticcheck = true,
+                    directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+                    semanticTokens = true,
+                },
                 -- pyright = {},
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
