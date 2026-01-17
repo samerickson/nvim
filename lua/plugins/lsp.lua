2fe148bb77f58eda94ebf45ace0bfc7d10b32000
Sam Erickson <erickson.samuel@outlook.com>
2026-01-16 21:43:41 -0800

    refactor(lsp): remove redundant client check

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,10 +90,7 @@ return {
                 --    See `:help CursorHold` for information about when this is executed
                 --
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                if
-                    client
-                    and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
-                then
+                if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
                     local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
                     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                         buffer = event.buf,
