a9117e6277ed67b9cba11f4c70ffd34c83832b1f
Sam Erickson <erickson.samuel@outlook.com>
2026-02-08 15:57:14 -0800

    feat: remove unused plugins and update

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -6,9 +6,7 @@ return {
         opts = {
             library = {
                 { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-                { path = 'LazyVim', words = { 'LazyVim' } },
                 { path = 'snacks.nvim', words = { 'Snacks' } },
-                { path = 'lazy.nvim', words = { 'LazyVim' } },
             },
         },
     },
--- lua/plugins/sleuth.lua
+++
@@ -1,6 +0,0 @@
-return {
-    {
-        'tpope/vim-sleuth',
-        event = { 'BufReadPre', 'BufNewFile' },
-    },
-}
