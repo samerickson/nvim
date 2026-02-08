d96328f4d074ec5dbec897c0cd05c2224faaf177
Sam Erickson <erickson.samuel@outlook.com>
2026-02-08 15:56:56 -0800

    feat: update blink.cmp config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,95 +5,15 @@ local function has_words_before()
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
 end
 
----@type function?, function?
-local icon_provider, hl_provider
-
-local function get_kind_icon(CTX)
-    -- Evaluate icon provider
-    if not icon_provider then
-        local _, mini_icons = pcall(require, 'mini.icons')
-        if _G.MiniIcons then
-            icon_provider = function(ctx)
-                local is_specific_color = ctx.kind_hl and ctx.kind_hl:match '^HexColor' ~= nil
-                if ctx.item.source_name == 'LSP' then
-                    local icon, hl = mini_icons.get('lsp', ctx.kind or '')
-                    if icon then
-                        ctx.kind_icon = icon
-                        if not is_specific_color then
-                            ctx.kind_hl = hl
-                        end
-                    end
-                elseif ctx.item.source_name == 'Path' then
-                    ctx.kind_icon, ctx.kind_hl =
-                        mini_icons.get(ctx.kind == 'Folder' and 'directory' or 'file', ctx.label)
-                elseif ctx.item.source_name == 'Snippets' then
-                    ctx.kind_icon, ctx.kind_hl = mini_icons.get('lsp', 'snippet')
-                end
-            end
-        end
-        if not icon_provider then
-            local lspkind_avail, lspkind = pcall(require, 'lspkind')
-            if lspkind_avail then
-                icon_provider = function(ctx)
-                    if ctx.item.source_name == 'LSP' then
-                        local icon = lspkind.symbolic(ctx.kind, { mode = 'symbol' })
-                        if icon then
-                            ctx.kind_icon = icon
-                        end
-                    elseif ctx.item.source_name == 'Snippets' then
-                        local icon = lspkind.symbolic('snippet', { mode = 'symbol' })
-                        if icon then
-                            ctx.kind_icon = icon
-                        end
-                    end
-                end
-            end
-        end
-        if not icon_provider then
-            icon_provider = function() end
-        end
-    end
-    -- Evaluate highlight provider
-    if not hl_provider then
-        local highlight_colors_avail, highlight_colors = pcall(require, 'nvim-highlight-colors')
-        if highlight_colors_avail then
-            local kinds
-            hl_provider = function(ctx)
-                if not kinds then
-                    kinds = require('blink.cmp.types').CompletionItemKind
-                end
-                if ctx.item.kind == kinds.Color then
-                    local doc = vim.tbl_get(ctx, 'item', 'documentation')
-                    if doc then
-                        local color_item = highlight_colors_avail
-                            and highlight_colors.format(doc, { kind = kinds[kinds.Color] })
-                        if color_item and color_item.abbr_hl_group then
-                            if color_item.abbr then
-                                ctx.kind_icon = color_item.abbr
-                            end
-                            ctx.kind_hl = color_item.abbr_hl_group
-                        end
-                    end
-                end
-            end
-        end
-        if not hl_provider then
-            hl_provider = function() end
-        end
-    end
-    -- Call resolved providers
-    icon_provider(CTX)
-    hl_provider(CTX)
-    -- Return text and highlight information
-    return { text = CTX.kind_icon .. CTX.icon_gap, highlight = CTX.kind_hl }
-end
-
 return {
     {
         'saghen/blink.cmp',
         version = '^1',
         event = { 'InsertEnter', 'CmdlineEnter' },
         opts = {
+            appearance = {
+                nerd_font_variant = 'mono',
+            },
             keymap = {
                 ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
                 ['<Up>'] = { 'select_prev', 'fallback' },
@@ -130,7 +50,7 @@ return {
                     default = { 'lsp', 'path', 'snippets', 'buffer' },
                 },
             },
-            fuzzy = { implementation = 'prefer_rust' },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
             completion = {
                 list = { selection = { preselect = false, auto_insert = false } },
                 menu = {
@@ -141,16 +61,6 @@ return {
                     winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
                     draw = {
                         treesitter = { 'lsp' },
-                        components = {
-                            kind_icon = {
-                                text = function(ctx)
-                                    return get_kind_icon(ctx).text
-                                end,
-                                highlight = function(ctx)
-                                    return get_kind_icon(ctx).highlight
-                                end,
-                            },
-                        },
                     },
                 },
                 accept = {
@@ -158,7 +68,7 @@ return {
                 },
                 documentation = {
                     auto_show = true,
-                    auto_show_delay_ms = 0,
+                    auto_show_delay_ms = 500,
                     window = {
                         border = 'rounded',
                         winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
