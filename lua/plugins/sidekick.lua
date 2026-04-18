1ec0ff19481b7f567a75ee692cb8f2633b1383e9
Sam Erickson <samericksn@gmail.com>
2026-04-17 18:13:23 -0700

    feat(sidekick): update keybindings

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -13,22 +13,18 @@ return {
     },
     keys = {
         {
-            '<tab>',
-            function()
-                -- if there is a next edit, jump to it, otherwise apply it if any
-                if not require('sidekick').nes_jump_or_apply() then
-                    return '<Tab>' -- fallback to normal tab
-                end
-            end,
-            expr = true,
-            desc = 'Goto/Apply Next Edit Suggestion',
-        },
-        {
             '<leader>as',
             function()
                 require('sidekick.cli').select()
             end,
             desc = 'Sidekick Select CLI',
+        },
+        {
+            '<leader>ad',
+            function()
+                require('sidekick.cli').close()
+            end,
+            desc = 'Sidekick Close',
         },
         {
             '<leader>af',
@@ -56,7 +52,15 @@ return {
         {
             '<a-/>',
             function()
-                require('sidekick.cli').toggle()
+                require('sidekick.cli').toggle { name = 'codex', focus = true }
+            end,
+            mode = { 'n', 'x', 'i', 't' },
+            desc = 'Sidekick Switch Toggle',
+        },
+        {
+            '<a-.>',
+            function()
+                require('sidekick.cli').focus { name = 'codex', focus = true }
             end,
             mode = { 'n', 'x', 'i', 't' },
             desc = 'Sidekick Switch Focus',
