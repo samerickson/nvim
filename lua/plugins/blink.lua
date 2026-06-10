01b4b8b874242de2b413d92dd88065f70b4d3a80
Sam Erickson <erickson.samuel@outlook.com>
2026-06-09 19:47:54 -0700

    chore: remove already converted files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,5 @@
 -- Most of this file has been taken from https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/blink.lua#L130
 
-local function has_words_before()
-    local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-end
 
 ---@module 'lazy'
 ---@type LazySpec
