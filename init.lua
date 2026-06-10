3d613a6a19d4e0dfe356df9df5507c5335f9fe6f
Sam Erickson <erickson.samuel@outlook.com>
2026-06-09 19:42:29 -0700

    feat: remove lazy.nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    -- stylua: ignore
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath })
-end
-
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require 'options'
 require 'keymaps'
@@ -15,44 +7,3 @@ if not require('samerickson.utils').is_windows then
     require('vim._core.ui2').enable {}
 end
 
-require('lazy').setup {
-    spec = {
-        { import = 'plugins' },
-    },
-    defaults = {
-        lazy = true,
-        version = false,
-    },
-    install = {
-        missing = true,
-        colorscheme = { 'kanagawa', 'habamax' },
-    },
-    checker = { enabled = false },
-    change_detection = {
-        notify = false,
-    },
-    dev = {
-        path = '~/dev/nvim-plugins',
-        patterns = { 'samerickson' },
-        fallback = true,
-    },
-    performance = {
-        cache = { enabled = true },
-        reset_packpath = true,
-        rtp = {
-            disabled_plugins = {
-                '2html_plugin',
-                'gzip',
-                'matchit',
-                'matchparen',
-                'netrwPlugin',
-                'netrwSettings',
-                'netrwFileHandlers',
-                'tarPlugin',
-                'tohtml',
-                'tutor',
-                'zipPlugin',
-            },
-        },
-    },
-}
