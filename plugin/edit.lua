ec56841a3e9f8233a1ecac0968f02809ac8db1b4
Sam Erickson <erickson.samuel@outlook.com>
2026-06-16 20:33:09 -0700

    feat: migrate git signs and re-add guess-indent

--- plugin/edit.lua
+++ plugin/edit.lua
@@ -1,2 +1,30 @@
-vim.pack.add { { src = 'https://github.com/NMAC427/guess-indent.nvim' } }
+vim.pack.add {
+    'https://github.com/NMAC427/guess-indent.nvim',
+    'https://github.com/lewis6991/gitsigns.nvim',
+}
+
 require('guess-indent').setup {}
+require('gitsigns').setup {
+    -- signs = {
+    --     add = { text = '+' }, ---@diagnostic disable-line: missing-fields
+    --     change = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    --     delete = { text = '_' }, ---@diagnostic disable-line: missing-fields
+    --     topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
+    --     changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
+    -- },
+    signs = {
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '▎' },
+        untracked = { text = '▎' },
+    },
+    signs_staged = {
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '▎' },
+    },
+}
