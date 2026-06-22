deb20659ff50e114447abbcbecfd53b72ae2877f
Sam Erickson <erickson.samuel@outlook.com>
2026-06-22 14:25:13 -0700

    feat(keymaps): clean up keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,13 +1,13 @@
 local map = vim.keymap.set
 
 -- Quit all
-map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All' })
+map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit All', silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
-map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window', remap = true })
-map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window', remap = true })
-map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window', remap = true })
-map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window', remap = true })
+map('n', '<C-h>', '<C-w>h', { desc = 'Go to Left Window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Go to Lower Window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Go to Upper Window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Go to Right Window' })
 
 -- Resize window using <ctrl> arrow keys
 map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
@@ -19,8 +19,6 @@ map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window W
 map('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
 map('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
 map('n', '<A-Down>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
--- map('n', '<A-Up>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
--- map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
 map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
 map('v', '<A-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move Down' })
 map('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move Up' })
@@ -28,9 +26,6 @@ map('v', '<A-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", {
 -- Shell (like) mappings
 map('i', '<C-a>', '<C-o>^', { desc = 'Move to begining of text on line' })
 map('i', '<A-f>', '<A-w>', { desc = 'Move forward a word' })
-
--- Mapping already exists/works
--- map('i', '<A-b>', '<A-b>', { desc = 'Move forward a word' })
 
 map('n', '<C-g>', function()
     vim.cmd ':let @+ = expand("%")'
@@ -59,7 +54,7 @@ map('n', '[e', diagnostic_jump(false, 'ERROR'), { desc = 'Prev Error' })
 map('n', ']w', diagnostic_jump(true, 'WARN'), { desc = 'Next Warning' })
 map('n', '[w', diagnostic_jump(false, 'WARN'), { desc = 'Prev Warning' })
 
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit Terminal Mode' })
+map('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit Terminal Mode' })
 map('t', '<c-/>', '<cmd>close<cr>', { desc = 'Close terminal' })
 map('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
 map('t', '<c-k>', '<C-\\><C-n><C-w>k', { desc = 'Window up' })
@@ -68,18 +63,4 @@ map({ 'v', 'x' }, '<leader>t', function()
     require('samerickson.translate').translate_selection 'fr'
 end, { desc = 'Translate selection to French' })
 
-map('n', 'gF', function()
-    local qf_data = vim.fn.getqflist({
-        lines = { vim.fn.getline '.' },
-    }).items[1]
-
-    if qf_data.valid == 1 then
-        vim.cmd.buffer(qf_data.bufnr)
-        vim.api.nvim_win_set_cursor(0, { qf_data.lnum, qf_data.col - 1 })
-    end
-end)
-
-map('n', 'Q', 'q', { noremap = true, silent = true })
-map('n', 'q', '', { noremap = true, silent = true })
-
-map('n', 'gx', require('samerickson.utils').open_link, { desc = 'Open with system app' })
+map('n', 'gx', require('samerickson.utils').open_link, { desc = 'Open with system app', silent = true })
