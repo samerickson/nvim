fc5cf4cb3aad1c49fe49e5c045fe68134ff7b78d
Sam Erickson <erickson.samuel@outlook.com>
2026-06-27 17:15:47 -0700

    feat: clean up blink config

--- plugin/blink.lua
+++ plugin/blink.lua
@@ -1,14 +1,8 @@
-local function has_words_before()
-    local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-end
-
-local gh = require('samerickson.pack').gh
 local on_plugin_update = require('samerickson.pack').on_plugin_update
 
 vim.pack.add {
-    { src = gh 'saghen/blink.lib', setup = false },
-    { src = gh 'saghen/blink.cmp' },
+    { src = 'https://github.com/saghen/blink.lib', setup = false },
+    { src = 'https://github.com/saghen/blink.cmp' },
 }
 
 require('blink.cmp').setup {
@@ -16,37 +10,13 @@ require('blink.cmp').setup {
         nerd_font_variant = 'mono',
     },
     keymap = {
-        ['<C-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-N>'] = { 'select_next', 'show' },
-        ['<C-P>'] = { 'select_prev' },
-        ['<C-J>'] = { 'select_next', 'fallback' },
-        ['<C-K>'] = { 'select_prev', 'fallback' },
-        ['<C-U>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-D>'] = { 'scroll_documentation_down', 'fallback' },
-        ['<C-e>'] = { 'hide', 'fallback' },
         ['<CR>'] = { 'accept', 'fallback' },
-        ['<Tab>'] = {
-            'select_next',
-            'snippet_forward',
-            function(cmp)
-                if has_words_before() or vim.api.nvim_get_mode().mode == 'c' then
-                    return cmp.show()
-                end
-            end,
-            'fallback',
-        },
-        ['<S-Tab>'] = {
-            'select_prev',
-            'snippet_backward',
-            function(cmp)
-                if vim.api.nvim_get_mode().mode == 'c' then
-                    return cmp.show()
-                end
-            end,
-            'fallback',
-        },
+        ['<C-\\>'] = { 'hide', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'show' },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        ['<C-p>'] = { 'select_prev' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
     },
     sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
@@ -79,5 +49,6 @@ require('blink.cmp').setup {
 }
 
 on_plugin_update('blink.cmp', function()
+    ---@diagnostic disable-next-line: undefined-field
     require('blink.cmp').build():pwait(60000)
 end)
