a8117b12346bbfe300bb7313f6c4687b73782320
Sam Erickson <erickson.samuel@outlook.com>
2026-07-05 11:48:23 -0700

    feat: add keymap to restart nvim and retain buffers

--- init.lua
+++ init.lua
@@ -43,3 +43,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.hl.hl_op()
     end,
 })
+
+vim.keymap.set('n', '<leader>R', function()
+    local session = vim.fn.stdpath 'state' .. '/restart_session.vim'
+    vim.cmd('mksession! ' .. vim.fn.fnameescape(session))
+    vim.cmd('restart source ' .. vim.fn.fnameescape(session))
+end, { desc = 'Restart Neovim' })
