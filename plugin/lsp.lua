f1c1495844121b5676aa352089bc5e8ae38698c2
Sam Erickson <erickson.samuel@outlook.com>
2026-07-15 13:46:03 -0700

    fix: luals giving vim undefined warnings

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -170,3 +170,23 @@ vim.lsp.config('vtsls', {
         'vue',
     },
 })
+
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            runtime = {
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                globals = { 'vim' },
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file('', true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
