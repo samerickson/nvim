743d86285ba4be47d3407b45118a28e21cb61e57
Sam Erickson <erickson.samuel@outlook.com>
2026-07-16 07:40:20 -0700

    performance: load blink later in the startup process

--- plugin/blink.lua
+++ plugin/blink.lua
@@ -5,48 +5,53 @@ vim.pack.add {
     { src = 'https://github.com/saghen/blink.cmp' },
 }
 
-require('blink.cmp').setup {
-    appearance = {
-        nerd_font_variant = 'mono',
-    },
-    keymap = {
-        ['<CR>'] = { 'accept', 'fallback' },
-        ['<C-\\>'] = { 'hide', 'fallback' },
-        ['<C-n>'] = { 'select_next', 'show' },
-        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['<C-p>'] = { 'select_prev' },
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-    },
-    sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
-    completion = {
-        list = { selection = { preselect = false, auto_insert = false } },
-        menu = {
-            auto_show = function(ctx)
-                return ctx.mode ~= 'cmdline'
-            end,
-            border = 'rounded',
-            winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
-            draw = {
-                treesitter = { 'lsp' },
+vim.api.nvim_create_autocmd('InsertEnter', {
+    once = true,
+    callback = function()
+        require('blink.cmp').setup {
+            appearance = {
+                nerd_font_variant = 'mono',
             },
-        },
-        accept = {
-            auto_brackets = { enabled = false },
-        },
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 500,
-            window = {
-                border = 'rounded',
-                winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
+            keymap = {
+                ['<CR>'] = { 'accept', 'fallback' },
+                ['<C-\\>'] = { 'hide', 'fallback' },
+                ['<C-n>'] = { 'select_next', 'show' },
+                ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+                ['<C-p>'] = { 'select_prev' },
+                ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+                ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
             },
-        },
-    },
-}
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
+            completion = {
+                list = { selection = { preselect = false, auto_insert = false } },
+                menu = {
+                    auto_show = function(ctx)
+                        return ctx.mode ~= 'cmdline'
+                    end,
+                    border = 'rounded',
+                    winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
+                    draw = {
+                        treesitter = { 'lsp' },
+                    },
+                },
+                accept = {
+                    auto_brackets = { enabled = false },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                    window = {
+                        border = 'rounded',
+                        winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
+                    },
+                },
+            },
+        }
+    end,
+})
 
 on_plugin_update('blink.cmp', function()
     ---@diagnostic disable-next-line: undefined-field
