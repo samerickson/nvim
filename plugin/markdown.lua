ba446ccc5c1217269e7dae09d87f803aca810ff9
Sam Erickson <erickson.samuel@outlook.com>
2026-07-16 20:26:33 -0700

    feat: remove markdown previewer

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -2,22 +2,7 @@ local pack = require 'samerickson.pack'
 
 vim.pack.add {
     'https://github.com/selimacerbas/live-server.nvim',
-    'https://github.com/selimacerbas/markdown-preview.nvim',
     'https://github.com/MeanderingProgrammer/render-markdown.nvim',
-}
-
-pack.on_plugin_update('markdown-preview', function()
-    vim.fn['mkdp#util#install']()
-end)
-
-require('markdown_preview').setup {
-    -- all optional; sane defaults shown
-    instance_mode = 'takeover', -- "takeover" (one tab) or "multi" (tab per instance)
-    port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
-    browser = { 'cmd.exe', '/C', 'start', '""', 'firefox' },
-    open_browser = true,
-    default_theme = 'dark', -- "dark" or "light"; initial preview theme
-    debounce_ms = 300,
 }
 
 require('render-markdown').setup {
